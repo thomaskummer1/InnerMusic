088394d263a77a919c7127718d51ae5d48f58ca7
thomaskummer1 <twk23@outlook.com>
2024-04-13 15:20:56 -0400

    converting to chakra ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import {
+  Box,
+} from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-    <Navbar />
-      <div className='content'>
-      <Home />
-      </div>
-    </div>
+    <Box>
+      <Navbar />
+      <Home/>
+    </Box>   
   );
 }
 
