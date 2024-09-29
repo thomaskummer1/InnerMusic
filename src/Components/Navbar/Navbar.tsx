d3aae0409147a773f0167054d5276c08a47cae8b
thomaskummer1 <twk23@outlook.com>
2024-09-29 12:58:27 -0400

    nav bar work

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -2,20 +2,26 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <nav>
-      <img id="logo" src="images/Logo.jpeg" height="200px" />
-      <h1>Inner Music</h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/SignIn">Sign In</Link>
-        </li>
-        <li>
-          <Link to="/Profile">Profile</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-light bg-light">
+      <Link
+        className="no-link"
+        // style={{ color: "#28245e", textDecoration: "none" }}
+        to="/"
+      >
+        <img id="logo" src="images/Logo.png" width="68" height="50" />
+        Inner Music
+      </Link>
+
+      <form className="d-flex">
+        <input type="search" placeholder="Search" aria-label="Search" />
+        <button type="submit">Search</button>
+      </form>
+      <Link to="/SignIn">
+        <button>Sign In</button>
+      </Link>
+      <Link to="/Profile">
+        <button>Profile</button>
+      </Link>
     </nav>
   );
 }
